88eaf9e8f71b51680b2f7834556c7a95e081d624
ebukathedev <ebuskelele@gmail.com>
2024-01-04 07:04:06 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./**/*.{html,js}"],
+	theme: {
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "1080px",
+			xl: "1200px",
+		},
+		extend: {
+			colors: {
+				"blue-light-50": "#F0F9FF",
+				"blue-light-700": "#026AA2",
+				"gray-25": "#FCFCFD",
+				"gray-100": "#F2F4F7",
+				"gray-300": "#D0D5DD",
+				"gray-500": "#667085",
+				"gray-700": "#344054",
+				"gray-900": "#101828",
+			},
+			backgroundImage: {
+				"gray-gradient-100":
+					"linear-gradient(45deg, #1D2939 0%, #475467 100%)",
+				"gray-gradient":
+					"linear-gradient(90deg, #475467 0%, #667085 100%)",
+			},
+			fontFamily: {
+				inter: ["Inter", "sans-serif"],
+				inspiration: ["Inspiration", "cursive"],
+			},
+		},
+	},
+	plugins: [],
+};
